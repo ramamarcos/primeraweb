65db988aaf5b77909eb5d53372bddaf479ead27b
ramamarcos <146309344+ramamarcos@users.noreply.github.com>
2023-12-25 07:17:27 -0300

    Create practica.js

---
+++ practica.js
@@ -0,0 +1,25 @@
+function primera() {
+    var barra = document.getElementById("barra")
+
+    if(barra.style.display === 'block' || barra.style.display === '') {
+        barra.style.display = 'none'
+    } else {
+        barra.style.display = 'block'
+    }
+}
+
+function interaccionar() {
+    var imag = document.getElementById("img")
+    
+    var src = imag.getAttribute("src")
+
+    if (src === 'off.gif') {
+        imag.setAttribute("src", 'on.gif')
+    } else {
+        imag.setAttribute("src", 'off.gif')
+    }
+}
+
+function alerta() {
+    window.alert("Este es un mensaje de alerta.")
+}
